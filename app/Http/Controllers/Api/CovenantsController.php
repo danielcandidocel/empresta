3778db2b6ff90dbcabe446b70deb650d02a89b31
Daniel Candido <danielcandidocel@gmail.com>
2021-06-16 13:13:56 -0300

    Testes de Rotas e Identação do Código

--- app/Http/Controllers/Api/CovenantsController.php
+++ app/Http/Controllers/Api/CovenantsController.php
@@ -4,7 +4,9 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use App\Simulator\SimulatorServiceContract;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 
 class CovenantsController extends Controller
 {
@@ -24,7 +26,7 @@ class CovenantsController extends Controller
 
     /**
      * Display a listing of the resource.
-     * @return \Illuminate\Http\Response
+     * @return JsonResponse
      */
     public function index()
     {
@@ -33,7 +35,7 @@ class CovenantsController extends Controller
 
     /**
      * Show the form for creating a new resource.
-     * @return \Illuminate\Http\Response
+     * @return Response
      */
     public function create()
     {
@@ -43,7 +45,7 @@ class CovenantsController extends Controller
     /**
      * Store a newly created resource in storage.
      * @param \Illuminate\Http\Request $request
-     * @return \Illuminate\Http\Response
+     * @return Response
      */
     public function store(Request $request)
     {
@@ -53,7 +55,7 @@ class CovenantsController extends Controller
     /**
      * Display the specified resource.
      * @param int $id
-     * @return \Illuminate\Http\Response
+     * @return Response
      */
     public function show($id)
     {
@@ -63,7 +65,7 @@ class CovenantsController extends Controller
     /**
      * Show the form for editing the specified resource.
      * @param int $id
-     * @return \Illuminate\Http\Response
+     * @return Response
      */
     public function edit($id)
     {
@@ -74,7 +76,7 @@ class CovenantsController extends Controller
      * Update the specified resource in storage.
      * @param \Illuminate\Http\Request $request
      * @param int $id
-     * @return \Illuminate\Http\Response
+     * @return Response
      */
     public function update(Request $request, $id)
     {
@@ -84,7 +86,7 @@ class CovenantsController extends Controller
     /**
      * Remove the specified resource from storage.
      * @param int $id
-     * @return \Illuminate\Http\Response
+     * @return Response
      */
     public function destroy($id)
     {
